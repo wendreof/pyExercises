134cfe4e71f6eee130842186bf66d6c52a3069da
wlf <wendreolf@gmail.com>
2021-05-05 00:14:46 -0300

    ex 03 and 04

---
+++ ex003.py
@@ -0,0 +1,3 @@
+n1 = int(input('Type some number: '))
+n2 = int(input('Type some other number: '))
+print('{0} + {1} is: {2} '.format(n1, n2, n1+n2))
---
+++ ex004.py
@@ -0,0 +1,6 @@
+n1 = input('Type some number: ')
+print('Is decimal? {} '.format(n1.isdecimal()))
+print('Is space? {} '.format(n1.isspace()))
+print('Is lower? {} '.format(n1.islower()))
+print('Is isalnum? {} '.format(n1.isalnum()))
+print('Is alpha? {} '.format(n1.isalpha()))
